5741425f503d696f5c01d6ee8c30268d3340b0dc
huobb0 <zsombor.kovacs@gmail.com>
2019-04-27 12:18:06 +0200

    Initial Commit

---
+++ SecureCommunications/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SecureCommunications
+//
+//  Created by Zsombor on 2019. 04. 27..
+//  Copyright © 2019. Zsombor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SecureCommunications/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  SecureCommunications
+//
+//  Created by Zsombor on 2019. 04. 27..
+//  Copyright © 2019. Zsombor. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
